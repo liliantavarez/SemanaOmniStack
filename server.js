c3dc8c2878a6b31c083e91dc7df15c3f2f7c2044
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2021-02-11 00:44:51 -0300

    SemanaOmniStack

---
+++ server.js
@@ -0,0 +1,12 @@
+//vamos falar de coisas basicas da programação em java script
+
+//variaveis, tipos = 
+
+const xicara = {
+    cor:"branco",
+    tamanho: 10,
+    receber() {
+        
+    }
+
+}
